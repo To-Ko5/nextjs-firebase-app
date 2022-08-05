d1d157275b8f926099380bb9406e561dc7733f4e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-05 21:45:26 +0900

    Add: button component

---
+++ components/common/button.tsx
@@ -0,0 +1,18 @@
+import React, { ButtonHTMLAttributes, FC, ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+const Button: FC<Props> = ({ children, ...props }) => {
+  return (
+    <button
+      className="px-4 py-2 rounded-full bg-blue-500 text-gray-50"
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next'
+import Button from '../components/common/Button'
 
 const LoginPage: NextPage = () => {
-  return <>LOGIN</>
+  return (
+    <>
+      <h1>LOGIN</h1>
+      <Button
+        type="button"
+        onClick={() => {
+          alert()
+        }}
+      >
+        ログイン
+      </Button>
+    </>
+  )
 }
 
 export default LoginPage
