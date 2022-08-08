ede60e345e13510ef1b3ce46e021eb3802f1abe4
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-08 21:24:08 +0900

    Add: ログインの処理を追加

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,19 @@
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { auth } from '../firebase/client'
 import type { NextPage } from 'next'
-import Button from '../components/common/Button'
+import Button from '../components/common/button'
 
 const LoginPage: NextPage = () => {
+  const login = () => {
+    const provider = new GoogleAuthProvider()
+    signInWithPopup(auth, provider).then((result) => {
+      alert(`${result.user.displayName}さんこんにちは`)
+    })
+  }
   return (
     <>
       <h1>LOGIN</h1>
-      <Button
-        type="button"
-        onClick={() => {
-          alert()
-        }}
-      >
+      <Button type="button" onClick={() => login()}>
         ログイン
       </Button>
     </>
