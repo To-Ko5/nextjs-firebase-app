a3130dd8bd188cc318fe3d45fd33ad8b798b839c
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-09 21:11:28 +0900

    Add: formを作成

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,28 +1,77 @@
+import { useForm } from 'react-hook-form'
 import Button from '../components/common/button'
 
-const createAccount = () => {
+type User = {
+  name: string
+  nickname: string
+  profile: string
+}
+
+const CreateAccount = () => {
+  const { register, handleSubmit } = useForm<User>()
+
+  const submit = (data: User) => {
+    console.log(data)
+  }
+
   return (
     <>
       <h1>新規登録</h1>
 
-      <div>
-        <label htmlFor="name">名前</label>
-        <input type="text" name="name" id="name" />
-      </div>
+      <form onSubmit={handleSubmit(submit)}>
+        <div>
+          <label htmlFor="name">名前</label>
+          <input
+            {...register('name', {
+              required: '必須入力',
+              maxLength: {
+                value: 50,
+                message: '最大50文字です'
+              }
+            })}
+            type="text"
+            name="name"
+            id="name"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="nickname">ニックネーム</label>
-        <input type="text" name="nickname" id="nickname" />
-      </div>
+        <div>
+          <label htmlFor="nickname">ニックネーム</label>
+          <input
+            {...register('nickname', {
+              required: '必須入力',
+              maxLength: {
+                value: 50,
+                message: '最大50文字です'
+              }
+            })}
+            type="text"
+            name="nickname"
+            id="nickname"
+          />
+        </div>
 
-      <div>
-        <label htmlFor="profile">プロフィール</label>
-        <textarea name="profile" id="profile" cols={30} rows={10}></textarea>
-      </div>
+        <div>
+          <label htmlFor="profile">プロフィール</label>
+          <textarea
+            {...register('profile', {
+              required: '必須入力',
+              maxLength: {
+                value: 250,
+                message: '最大250文字です'
+              }
+            })}
+            name="profile"
+            id="profile"
+            cols={30}
+            rows={10}
+          ></textarea>
+        </div>
 
-      <Button>アカウント作成</Button>
+        <Button>アカウント作成</Button>
+      </form>
     </>
   )
 }
 
-export default createAccount
+export default CreateAccount
