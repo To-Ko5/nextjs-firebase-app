7d9b4ca38ff428d8037a5b1750b545a5b243c351
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-09 20:58:05 +0900

    Add: 入力フォーム

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import Button from '../components/common/button'
 
 const createAccount = () => {
   return (
     <>
       <h1>新規登録</h1>
+
+      <div>
+        <label htmlFor="name">名前</label>
+        <input type="text" name="name" id="name" />
+      </div>
+
+      <div>
+        <label htmlFor="nickname">ニックネーム</label>
+        <input type="text" name="nickname" id="nickname" />
+      </div>
+
+      <div>
+        <label htmlFor="profile">プロフィール</label>
+        <textarea name="profile" id="profile" cols={30} rows={10}></textarea>
+      </div>
+
+      <Button>アカウント作成</Button>
     </>
   )
 }
