4ea732d0a4e15955078d34880f9b02c73df2bdb6
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-09 20:45:18 +0900

    Update: ロジックをlibに移動

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,17 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-import { auth } from '../firebase/client'
 import type { NextPage } from 'next'
+import { login, logout } from '../lib/auth'
 import Button from '../components/common/button'
 
 const LoginPage: NextPage = () => {
-  const login = () => {
-    const provider = new GoogleAuthProvider()
-    signInWithPopup(auth, provider).then((result) => {
-      alert(`${result.user.displayName}さんこんにちは`)
-    })
-  }
   return (
     <>
       <h1>LOGIN</h1>
-      <Button type="button" onClick={() => login()}>
+      <Button type="button" onClick={login}>
         ログイン
+      </Button>
+
+      <Button type="button" onClick={logout}>
+        ログアウト
       </Button>
     </>
   )
