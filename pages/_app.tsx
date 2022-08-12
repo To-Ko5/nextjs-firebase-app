20cbe58d8d84ff2a4477a90a9e8e124ec375ebaa
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-12 23:00:35 +0900

    Add: AuthProviderを読み込み

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthPovider } from '../context/auth'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthPovider>
+      <Component {...pageProps} />
+    </AuthPovider>
+  )
 }
 
 export default MyApp
