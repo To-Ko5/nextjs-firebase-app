d8aa9e5ca90b7e154718cf5f94352b6fb6ff26c8
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-16 20:48:34 +0900

    Add: algolia components

--- pages/search-post.tsx
+++ pages/search-post.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import algoliasearch from 'algoliasearch/lite'
+import { InstantSearch, SearchBox } from 'react-instantsearch-hooks-web'
+
+const searchClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIS_KEY as string,
+  process.env.NEXT_PUBLIC_ALGOLIS_SEARCH_KEY as string
+)
 
 const SearchPost = () => {
   return (
