a82f73476bcdc7dd2f0cc985c157fcf017a7ccce
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-16 20:53:03 +0900

    Add: 検索の表示を追加

--- pages/search-post.tsx
+++ pages/search-post.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import algoliasearch from 'algoliasearch/lite'
-import { InstantSearch, SearchBox } from 'react-instantsearch-hooks-web'
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-hooks-web'
+import { Post } from '../types/post'
+import { HitsProps } from 'react-instantsearch-hooks-web/dist/es/ui/Hits'
 
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIS_KEY as string,
   process.env.NEXT_PUBLIC_ALGOLIS_SEARCH_KEY as string
 )
 
+const Hit: HitsProps<Post>['hitComponent'] = ({ hit }) => {
+  return <div>{hit.title}</div>
+}
+
 const SearchPost = () => {
   return (
     <div>
       <h1>検索ページ</h1>
+      <InstantSearch indexName="posts" searchClient={searchClient}>
+        <SearchBox />
+        <Hits<Post> hitComponent={Hit} />
+      </InstantSearch>
     </div>
   )
 }
