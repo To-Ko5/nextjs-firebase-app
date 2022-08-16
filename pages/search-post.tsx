3a71ff750dc134e5debf67ee3b1a9de3d13e2f33
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-16 21:23:45 +0900

    Add: 検索結果がない時の処理を追加

--- pages/search-post.tsx
+++ pages/search-post.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import algoliasearch from 'algoliasearch/lite'
 import { debounce } from 'debounce'
 import {
@@ -6,7 +6,8 @@ import {
   SearchBox,
   Hits,
   HitsProps,
-  SearchBoxProps
+  SearchBoxProps,
+  useInstantSearch
 } from 'react-instantsearch-hooks-web'
 import { Post } from '../types/post'
 
@@ -19,6 +20,20 @@ const Hit: HitsProps<Post>['hitComponent'] = ({ hit }) => {
   return <div>{hit.title}</div>
 }
 
+const NoResultsBoundary = ({ children }: { children: ReactNode }) => {
+  const { results } = useInstantSearch()
+
+  if (!results.__isArtificial && results.nbHits === 0) {
+    return (
+      <>
+        <div>【{results.query}】検索結果はありませんでした</div>
+      </>
+    )
+  }
+
+  return <>{children}</>
+}
+
 const SearchPost = () => {
   const search: SearchBoxProps['queryHook'] = (query, hook) => {
     hook(query)
@@ -28,7 +43,9 @@ const SearchPost = () => {
       <h1>検索ページ</h1>
       <InstantSearch indexName="posts" searchClient={searchClient}>
         <SearchBox queryHook={debounce(search, 500)} />
-        <Hits<Post> hitComponent={Hit} />
+        <NoResultsBoundary>
+          <Hits<Post> hitComponent={Hit} />
+        </NoResultsBoundary>
       </InstantSearch>
     </div>
   )
