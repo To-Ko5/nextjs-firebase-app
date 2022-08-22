2a2ed8cc447f7e966fc8a04029ba233877698681
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-22 18:23:22 +0900

    Change: カスタムフックに変更

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,24 +1,17 @@
+import { format } from 'date-fns'
 import { doc, getDoc } from 'firebase/firestore'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-import useSWR from 'swr/immutable'
 import { db } from '../../firebase/client'
+import { useUser } from '../../hooks/user'
 import { Post } from '../../types/post'
-import { User } from '../../types/user'
 
 const PostDetail = () => {
   const router = useRouter()
   const [post, setPost] = useState<Post>()
   const postId = router.query.id
+  const user = useUser(post?.authorId)
 
-  const { data: user } = useSWR<User>(
-    post?.authorId && `users/${post.authorId}`,
-    async () => {
-      const ref = doc(db, `users/${post?.authorId}`)
-      const response = await getDoc(ref)
-      return response.data() as User
-    }
-  )
   useEffect(() => {
     const ref = doc(db, `posts/${postId}`)
     getDoc(ref).then((snap) => {
@@ -35,7 +28,13 @@ const PostDetail = () => {
       <div className="container">
         <h1 className="font-bold text-lg">{post.title}</h1>
 
-        <div className="flex">{user && <p>{user.name}</p>}</div>
+        <div className="flex">
+          <div className=""></div>
+          <div className="flex-1">
+            {user && <p>{user.name}</p>}
+            <p>{format(post.createdAt, 'yyyy年MM月dd日')}</p>
+          </div>
+        </div>
 
         <p className="">{post.body}</p>
       </div>
