93e48a41f55c5931d3508433c49d15beb5fff585
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-24 21:11:19 +0900

    Change: 処理をgetStaticPropsに変更

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,28 +1,20 @@
 import { format } from 'date-fns'
 import { doc, getDoc } from 'firebase/firestore'
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { db } from '../../firebase/client'
+import { adiminDB } from '../../firebase/server'
 import { useUser } from '../../hooks/user'
 import { Post } from '../../types/post'
 
-const PostDetail = () => {
-  const router = useRouter()
-  const [post, setPost] = useState<Post>()
-  const postId = router.query.id
+const PostDetail = ({
+  post
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const user = useUser(post?.authorId)
 
-  useEffect(() => {
-    if (postId) {
-      const ref = doc(db, `posts/${postId}`)
-      getDoc(ref).then((snap) => {
-        setPost(snap.data() as Post)
-      })
-    }
-  }, [postId])
-
   if (!post) {
-    return null
+    return <p>記事がありません。</p>
   }
 
   return (
@@ -45,6 +37,28 @@ const PostDetail = () => {
       </div>
     </>
   )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking'
+  }
+}
+
+export const getStaticProps: GetStaticProps<{ post: Post }> = async ({
+  params
+}) => {
+  const response = await adiminDB.doc(`posts/${params?.id}`).get()
+  const post = response.data() as Post
+  if (!post) {
+    return {
+      notFound: true
+    }
+  }
+  return {
+    props: { post }
+  }
 }
 
 export default PostDetail
