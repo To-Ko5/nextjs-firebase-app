23f63c68a39bca7e0c36970ccdef48d02acd0e00
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-25 10:37:59 +0900

    Add: revalidate

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,1 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+type Data =
+  | {
+      revalidated: boolean
+    }
+  | string
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  // Check for secret to confirm this is a valid request
+  // if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  //   return res.status(401).json({ message: 'Invalid token' })
+  // }
+  try {
+    // this should be the actual path not a rewritten path
+    // e.g. for "/blog/[slug]" this should be "/blog/post-1"
+    await res.revalidate(req.query.path as string)
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send('Error revalidating')
+  }
+}
