806e78f2a191cb8795501a9eecee593d85660093
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-08-29 20:39:43 +0900

    Add: logoを追加

--- components/common/layout.tsx
+++ components/common/layout.tsx
@@ -1,9 +1,14 @@
+import Image from 'next/image'
 import React, { ReactNode } from 'react'
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <header className="py-4 bg-blue-400"></header>
+      <header className="py-4 bg-blue-400">
+        <div className="container">
+          <Image src="/logo.svg" width={160} height={32} alt="logo" />
+        </div>
+      </header>
       <main>{children}</main>
 
       <footer className="py-4 bg-blue-400"></footer>
