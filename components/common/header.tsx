11bd4ac95352b88346245af083c39634b3783f1b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-09-04 21:27:20 +0900

    Add: sidebar component

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,37 +1,54 @@
+import { MenuIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '../../context/auth'
 import UserMenu from '../user/user-menu'
-import Button from './button'
+import Sidebar from './sidebar'
 
 const Header = () => {
   const { user, isLoading } = useAuth()
+  const [isSideber, setIsSidebarOpen] = useState<boolean>(true)
+
+  const closeModal = () => {
+    setIsSidebarOpen(false)
+  }
+
+  const openModal = () => {
+    setIsSidebarOpen(true)
+  }
 
   if (isLoading) {
     return null
   }
 
   return (
-    <header className="py-4 bg-blue-400 mb-10">
-      <div className="container">
-        <div className="flex items-center justify-between">
-          <Link href="/">
-            <a className="flex">
-              <Image src="/logo.svg" width={160} height={32} alt="logo" />
-            </a>
-          </Link>
-
-          {user ? (
-            <UserMenu />
-          ) : (
-            <Link href="/login">
-              <a>ログイン</a>
+    <>
+      <header className="py-4 bg-blue-400 mb-10">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <button className="p-2 mr-2" onClick={openModal}>
+              <MenuIcon className="w-5 h-5"></MenuIcon>
+            </button>
+            <Link href="/">
+              <a className="flex">
+                <Image src="/logo.svg" width={160} height={32} alt="logo" />
+              </a>
             </Link>
-          )}
+            <div className="flex-1"></div>
+            {user ? (
+              <UserMenu />
+            ) : (
+              <Link href="/login">
+                <a>ログイン</a>
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      <Sidebar isOpen={isSideber} closeModal={closeModal} />
+    </>
   )
 }
 
---
+++ components/common/sidebar.tsx
@@ -0,0 +1,59 @@
+import { Dialog, Transition } from '@headlessui/react'
+import Link from 'next/link'
+import { Fragment } from 'react'
+
+const Sidebar = ({
+  isOpen,
+  closeModal
+}: {
+  isOpen: boolean
+  closeModal: () => void
+}) => {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="fixed z-10 inset-0" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-y-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 -translate-x-full"
+                enterTo="opacity-100 translate-x-0"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-x-0"
+                leaveTo="opacity-0 -translate-x-full"
+              >
+                <Dialog.Panel className="w-80 fixed left-0 inset-y-0 bg-gray-50">
+                  <ul>
+                    <li>
+                      <Link href="">
+                        <a className="block px-2 py-4 border-b border-gray-500">
+                          メニュー
+                        </a>
+                      </Link>
+                    </li>
+                  </ul>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
+
+export default Sidebar
