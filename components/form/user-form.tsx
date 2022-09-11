a95f01b40103bfe9a0fedfb0aaf73e086c6276d6
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-09-11 17:46:24 +0900

    Change: avatarのトリミングを円形に変更

--- components/form/user-form.tsx
+++ components/form/user-form.tsx
@@ -88,6 +88,7 @@ const UserForm = ({ isEditMode }: { isEditMode?: boolean }) => {
                 image={selectedImage}
                 width={250}
                 height={250}
+                borderRadius={125}
                 border={50}
                 color={[255, 255, 255, 0.6]} // RGBA
                 scale={scale}
