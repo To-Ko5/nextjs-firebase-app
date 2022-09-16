cf11a9948f1162c3bfec1664e5f046cbbf3e4a5b
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-09-16 23:27:00 +0900

    Add: layoutを追加

--- pages/create-post.tsx
+++ pages/create-post.tsx
@@ -1,10 +1,18 @@
+import { ReactElement } from 'react'
+import Layout from '../components/common/layout'
 import PostForm from '../components/form/post-form'
-const CreatePost = () => {
+import { NextPageWithLayout } from './_app'
+
+const CreatePost: NextPageWithLayout = () => {
   return (
     <>
       <PostForm />
     </>
   )
+}
+
+CreatePost.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
 }
 
 export default CreatePost
