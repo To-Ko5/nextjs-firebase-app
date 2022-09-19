a642a74bc4bf85e4327fad8370c475b9dfc00596
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-09-19 10:22:52 +0900

    Change: linkを全体に変更

--- components/post/PostItemCard.tsx
+++ components/post/PostItemCard.tsx
@@ -7,30 +7,30 @@ const PostItemCard = ({ post }: { post: Post }) => {
   const user = useUser(post.authorId)
 
   return (
-    <div className="rounded-sm shadow p-4">
-      <p className="line-clamp-2">
-        <Link href={`/post/${post.id}`}>
-          <a>{post.title}</a>
-        </Link>
-      </p>
-      <div className="flex items-center">
-        {user && (
-          /* eslint-disable @next/next/no-img-element */
-          <div className="w-8 h-8 rounded-full overflow-hidden mr-4">
-            <img
-              src={user.avatarURL}
-              alt={user.nickname}
-              className="w-full h-full"
-            />
+    <div>
+      <Link href={`/post/${post.id}`}>
+        <a className="block rounded-sm shadow p-4">
+          <p className="line-clamp-2">{post.title}</p>
+          <div className="flex items-center">
+            {user && (
+              /* eslint-disable @next/next/no-img-element */
+              <div className="w-8 h-8 rounded-full overflow-hidden mr-4">
+                <img
+                  src={user.avatarURL}
+                  alt={user.nickname}
+                  className="w-full h-full"
+                />
+              </div>
+            )}
+            <div>
+              <p className="text-slate-500 text-sm">
+                {format(post.createdAt, 'yyyy年MM月dd日')}
+              </p>
+              {user ? <p className="truncate">{user.name}</p> : <p>...</p>}
+            </div>
           </div>
-        )}
-        <div>
-          <p className="text-slate-500 text-sm">
-            {format(post.createdAt, 'yyyy年MM月dd日')}
-          </p>
-          {user ? <p className="truncate">{user.name}</p> : <p>...</p>}
-        </div>
-      </div>
+        </a>
+      </Link>
     </div>
   )
 }
